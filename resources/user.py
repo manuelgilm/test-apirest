daa7ea42a466eca9b34484d7410fd3523bb4155d
Manuel Gil <manuelgilsitio@gmail.com>
2020-07-16 16:42:00 -0500

    here stable-baselines package is installing

--- resources/user.py
+++ resources/user.py
@@ -1,8 +1,6 @@
 from flask_restful import Resource 
-import json
-import tensorflow
 
 class User(Resource):
 
     def get(self):
-        return {'message':'Here is importing tensorflow package'}
+        return {'message':'Here is importing stable-baselines package'}
